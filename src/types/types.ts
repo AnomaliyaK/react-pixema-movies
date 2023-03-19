e5f87bb8a3176a7d89aab066de25d59f0a1f6631
Anastasiya Kachan <anomaliya@bk.ru>
2023-03-19 21:05:03 +0100

    feat (ui) add enum colors

---
+++ src/types/types.ts
@@ -0,0 +1,62 @@
+interface DataMovieApi {
+  Search: MovieApi[];
+  totalResults: number;
+  Response: boolean;
+}
+
+interface DataMovie {
+  movies: MovieApi[];
+  totalResults: number;
+  response: boolean;
+}
+
+interface MovieApi {
+  Title: string;
+  Year: number;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+
+interface Movie {
+  title: string;
+  year: number;
+  id: string;
+  type: string;
+  poster: string;
+}
+
+export type { DataMovieApi, DataMovie, MovieApi, Movie };
+
+// export interface MovieDetailsApi {
+//   Title: string;
+//   Year: string;
+//   Rated: string;
+//   Released: string;
+//   Runtime: string;
+//   Genre: string;
+//   Director: string;
+//   Writer: string;
+//   Actors: string;
+//   Plot: string;
+//   Language: string;
+//   Country: string;
+//   Awards: string;
+//   Poster: string;
+//   Ratings: RatingsApi[];
+//   Metascore: string;
+//   imdbRating: string;
+//   imdbVotes: string;
+//   imdbID: string;
+//   Type: string;
+//   DVD: string;
+//   BoxOffice: string;
+//   Production: string;
+//   Website: string;
+//   Response: string;
+// }
+
+// export interface RatingsApi {
+//   Source: string;
+//   Value: string;
+// }
