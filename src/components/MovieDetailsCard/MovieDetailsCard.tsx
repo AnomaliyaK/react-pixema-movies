49b5af89c8ec7166f678bde2caf2f7f0f971f78c
Anastasiya Kachan <anomaliya@bk.ru>
2023-03-26 21:51:38 +0200

    feat (components): add MoviedetailsCard component

---
+++ src/components/MovieDetailsCard/MovieDetailsCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MovieDetails } from 'types';
+
+interface MovieDetailsCardProps {
+  movieDetails: MovieDetails;
+}
+
+export const MovieDetailsCard = ({ movieDetails }: MovieDetailsCardProps) => {
+  return <div>MovieDetailsCard</div>;
+};
--- src/pages/MovieDetailsPage/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.tsx
@@ -1,10 +1,10 @@
 import { AsyncThunkAction, Dispatch, AnyAction } from '@reduxjs/toolkit';
 import { ErrorMessage, Loader } from 'components';
+import { MovieDetailsCard } from 'components/MovieDetailsCard/MovieDetailsCard';
 import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { fetchMovieDetails } from 'store/features/movieDetailsSlice/movieDetailsSlice';
 import { useAppDispatch, useAppSelector } from 'store/hooks/hooks';
-import { MovieDetails } from 'types';
 import { StyledDetailsPage } from './styles';
 
 export const MovieDetailsPage = () => {
@@ -27,6 +27,7 @@ export const MovieDetailsPage = () => {
       <h1>{title}</h1>
       {isLoading && <Loader />}
       {error && <ErrorMessage message={error} />}
+      {movieDetails && <MovieDetailsCard movieDetails={movieDetails} />}
       <button onClick={handleback}>Back</button>
     </StyledDetailsPage>
   );
