2b2966f902ec7165adf6b5d030cef1853dfebcfc
Anastasiya Kachan <anomaliya@bk.ru>
2023-03-29 12:00:56 +0200

    feat(page): feat SettingsPage

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React from "react";
+import { StyledSettingsPage } from "./styles";
 
 export const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  return (
+    <StyledSettingsPage>
+      <h1>SettingsPage</h1>
+      <div>
+        <h2>Profile</h2>
+        <p>Name</p>
+        <input type="text" />
+        <p>Email</p>
+        <input type="text" />
+      </div>
+      <div>
+        <h2>Password</h2>
+        <p>Password</p>
+        <input type="password" />
+        <p>New password</p>
+        <input type="password" />
+        <p>Confirm password</p>
+        <input type="password" />
+      </div>
+      <div>
+        <h2>Color mode</h2>
+        <p>Dark</p>
+        <p>use dark thema</p>
+      </div>
+      <div>
+        <button>Cancel</button>
+        <button>Save</button>
+      </div>
+    </StyledSettingsPage>
+  );
 };
---
+++ src/pages/SettingsPage/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { COLOR } from "ui";
+
+export const StyledSettingsPage = styled.div`
+  display: grid;
+  gap: 40px;
+  margin-left: 306px;
+  margin-top: 112px;
+  color: ${COLOR.WHITE};
+`;
